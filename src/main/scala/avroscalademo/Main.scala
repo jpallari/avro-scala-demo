bd162031e22f895986c04a23b0d45f9f5d983624
Jaakko Pallari <jaakkop@cakesolutions.net>
2016-10-12 23:22:50 +0100

    Load schema from file

--- src/main/scala/avroscalademo/Main.scala
+++ src/main/scala/avroscalademo/Main.scala
@@ -1,33 +1,51 @@
 package avroscalademo
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import org.apache.avro.Schema
 import com.sksamuel.avro4s.{SchemaFor, ToRecord, AvroOutputStream, AvroInputStream}
 
 import avroscalademo.v1.Person
 
-object Main {
-  implicit val personSchema: SchemaFor[Person] = SchemaFor[Person]
-  implicit val personToRecord: ToRecord[Person] = ToRecord[Person]
+object PersonSchema {
+  // Schema file as a input stream
+  private val schemaFile = getClass.getResourceAsStream("/avro/demo.avsc")
 
-  def main(args: Array[String]): Unit = {
-    val person = Person(name = "Harry", email = Seq("harryh@company.com", "h4rr1@hacker.xyz"))
-
-    val personAsBytes: Array[Byte] = {
-      val baos = new ByteArrayOutputStream
-      val output = AvroOutputStream.binary[Person](baos)
-      output.write(person)
-      output.close()
-      baos.toByteArray
-    }
-
-    val personFromBytes: Person = {
-      val in = new ByteArrayInputStream(personAsBytes)
-      val input = AvroInputStream.binary[Person](in)
-      input.iterator.toSeq.head
-    }
-
-    println("Schema: " + personSchema())
-    println("Person before serializing  : " + person)
-    println("Person after deserializing : " + personFromBytes)
+  // Schema parsed from the schema file
+  val personSchema: Schema = {
+    val schemaParser = new Schema.Parser
+    schemaParser.parse(schemaFile)
   }
+
+  // Schema provided implicitly
+  implicit val personSchemaImplicit: SchemaFor[Person] =
+    new SchemaFor[Person] {
+      override def apply(): Schema = personSchema
+    }
+
+  // Person to generic record converter
+  implicit val personToRecord: ToRecord[Person] = ToRecord[Person]
+}
+
+object Main extends App {
+  import PersonSchema._
+
+  val person = Person(name = "Harry", email = Seq("harryh@company.com", "h4rr1@hacker.xyz"))
+
+  val personAsBytes: Array[Byte] = {
+    val baos = new ByteArrayOutputStream
+    val output = AvroOutputStream.binary[Person](baos)
+    output.write(person)
+    output.close()
+    baos.toByteArray
+  }
+
+  val personFromBytes: Person = {
+    val in = new ByteArrayInputStream(personAsBytes)
+    val input = AvroInputStream.binary[Person](in)
+    input.iterator.toSeq.head
+  }
+
+  println("Schema: " + personSchema)
+  println("Person before serializing  : " + person)
+  println("Person after deserializing : " + personFromBytes)
 }
